076278ef812e296ee6a29f2a5a2ad231506d69f6
Ivan Andres Diaz <ivanandresdilox@gmail.com>
2023-07-09 21:40:13 -0500

    add read me, and replace deprecated method of expo-constants

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
 # CRUD Firebase
+
 CRUD with firebase and react native expo ⚛️
 
 ### Check out the video tutorial [here](https://www.youtube.com/watch?v=2lkfcEsTkg0)
+
 ![Frame 70](https://user-images.githubusercontent.com/43630417/167732099-642670f3-277c-4c2b-b043-d3f6dfa9cbde.png)
+
+## Running the project
+
+```
+    git clone https://github.com/betomoedano/CRUD_Firebase.git
+```
+
+```
+    cd CRUD_Firebase
+```
+
+Expo SDK and libreries are always updating their versions and deprecating others. before installing the libreries run.
+
+```
+    yarn add expo@latest
+```
+
+Next you can run:
+
+```
+    npx expo install --fix
+```
+
+Expo will show you what dependencies need to be updated. Install the dependencies expo suggest you. It is possible that there is cache and you have to run.
+
+```
+    yarn start --reset-cache
+```
--- src/screens/Add.js
+++ src/screens/Add.js
@@ -1,88 +1,89 @@
-import * as React from 'react';
-import * as RN from 'react-native';
-import { collection, addDoc } from 'firebase/firestore';
-import { database } from '../../config/fb';
-import { useNavigation } from '@react-navigation/native';
-import EmojiPicker from 'rn-emoji-keyboard';
+import * as React from "react";
+import * as RN from "react-native";
+import { collection, addDoc } from "firebase/firestore";
+import { database } from "../../config/fb";
+import { useNavigation } from "@react-navigation/native";
+import EmojiPicker from "rn-emoji-keyboard";
 
 export default function Add() {
-    const navigation = useNavigation();
-    const [isOpen, setIsOpen] = React.useState(false);
-    const [newItem, setNewItem] = React.useState({
-        emoji: '📷',
-        name: '',
-        price: 0,
-        isSold: false,
-        createdAt: new Date(),
-    });
+  const navigation = useNavigation();
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [newItem, setNewItem] = React.useState({
+    emoji: "📷",
+    name: "",
+    price: 0,
+    isSold: false,
+    createdAt: new Date(),
+  });
 
-    const handlePick = (emojiObject) => {
-        setNewItem({
-            ...newItem,
-            emoji: emojiObject.emoji,
-        });
-      /* example emojiObject = { 
+  const handlePick = (emojiObject) => {
+    setNewItem({
+      ...newItem,
+      emoji: emojiObject.emoji,
+    });
+  };
+  /* example emojiObject = { 
           "emoji": "❤️",
           "name": "red heart",
           "slug": "red_heart",
         }
       */
-    }
 
-    const onSend = async () => {
-        const docRef = await addDoc(collection(database, 'products'), newItem);
-        navigation.goBack();
-      }
+  const onSend = async () => {
+    const docRef = await addDoc(collection(database, "products"), newItem);
+    navigation.goBack();
+  };
 
-    return(
-        <RN.View style={styles.container}>
-            <RN.Text style={styles.title}>Sell a New Product</RN.Text>
-            <RN.Text onPress={() => setIsOpen(true)} style={styles.emoji}>{newItem.emoji}</RN.Text>
-            <EmojiPicker
-                onEmojiSelected={handlePick}
-                open={isOpen}
-                onClose={() => setIsOpen(false)} 
-            />
-                <RN.TextInput 
-                    onChangeText={(text) => setNewItem({...newItem, name: text})}
-                    style={styles.inputContainer} 
-                    placeholder='Product Name' 
-                />
-                <RN.TextInput 
-                    onChangeText={(text) => setNewItem({...newItem, price: text})}
-                    style={styles.inputContainer} 
-                   placeholder='$ Price' 
-                />
-            <RN.Button title='Publish' onPress={onSend}/>
-        </RN.View>
-    )
+  return (
+    <RN.View style={styles.container}>
+      <RN.Text style={styles.title}>Sell a New Product</RN.Text>
+      <RN.Text onPress={() => setIsOpen(true)} style={styles.emoji}>
+        {newItem.emoji}
+      </RN.Text>
+      <EmojiPicker
+        onEmojiSelected={handlePick}
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
+      <RN.TextInput
+        onChangeText={(text) => setNewItem({ ...newItem, name: text })}
+        style={styles.inputContainer}
+        placeholder="Product Name"
+      />
+      <RN.TextInput
+        onChangeText={(text) => setNewItem({ ...newItem, price: text })}
+        style={styles.inputContainer}
+        placeholder="$ Price"
+      />
+      <RN.Button title="Publish" onPress={onSend} />
+    </RN.View>
+  );
 }
 
 const styles = RN.StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-    },
-    title: {
-        fontSize: 32,
-        fontWeight: '700',
-    },
-    inputContainer: {
-        width: '90%',
-        padding: 13,
-        marginVertical: 6,
-        borderWidth: 1,
-        borderColor: '#ddd',
-        borderRadius: 6,
-      },
-    emoji: {
-        fontSize: 100,
-        borderWidth: 1,
-        borderColor: '#ddd',
-        borderRadius: 6,
-        padding: 10,
-        marginVertical: 6,
-    }
-
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "700",
+  },
+  inputContainer: {
+    width: "90%",
+    padding: 13,
+    marginVertical: 6,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 6,
+  },
+  emoji: {
+    fontSize: 100,
+    borderWidth: 1,
+    borderColor: "#ddd",
+    borderRadius: 6,
+    padding: 10,
+    marginVertical: 6,
+  },
 });
